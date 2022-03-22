a88765e59cb46d0968861bf48d0163e74d1ae701
Pacharapol Withayasakpunt <polv@polv.cc>
2022-03-22 13:03:24 +0700

    update

--- youtube/hide-suggestions.user.js
+++ youtube/hide-suggestions.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Hide the annoying YouTube suggestions
 // @author       polv
-// @match        https://www.youtube.com/watch
+// @match        https://www.youtube.com/watch?*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
@@ -12,62 +12,32 @@
 ;(function () {
   'use script'
 
-  /**
-   *
-   * @param {HTMLElement} selector
-   * @param {object} opts
-   * @param {HTMLElement} [opts.node]
-   * @param {(el: HTMLElement) => void} opts.action
-   */
-  function doAction(selector, opts) {
-    const el = /** @type {HTMLDivElement} */ (
-      (opts.node || document.body).querySelector(selector)
-    )
-    if (el) {
-      opts.action(el)
-    }
-  }
-
-  const SEL = {
-    selector: '#secondary',
-    display: 'block',
-    doHide() {
-      return doAction(this.selector, {
-        action: (el) => {
-          if (el.style.display !== 'none') {
-            this.display = el.style.display
-            el.style.display = 'none'
-          }
-        }
-      })
-    },
-    doToggle() {
-      return doAction(this.selector, {
-        action: (el) =>
-          el.style.display === 'none'
-            ? (el.style.display = this.display)
-            : (el.style.display = 'none')
-      })
-    }
-  }
-
-  SEL.doHide()
-
   window.addEventListener('keypress', (ev) => {
-    if (ev.key !== 's') {
-      return
-    }
+    if (ev.key !== 's') return
+
+    const el = document.querySelector('#secondary')
+    if (!el) return
 
     const target = /** @type {HTMLElement} */ (ev.target)
 
-    if (['INPUT', 'TEXTAREA'].includes(target.tagName.toLocaleUpperCase())) {
+    if (['INPUT', 'TEXTAREA'].includes(target.tagName.toLocaleUpperCase()))
       return
-    }
 
-    if (target.hasAttribute('contenteditable')) {
-      return
-    }
+    if (target.hasAttribute('contenteditable')) return
 
-    SEL.doToggle()
+    el.style.display = el.style.display === 'none' ? 'block' : 'none'
+  })
+
+  const beginObserver = new MutationObserver(() => {
+    const el = document.querySelector('#secondary')
+    if (!el) return
+
+    el.style.display = 'none'
+    beginObserver.disconnect()
+  })
+
+  beginObserver.observe(document.body, {
+    childList: true,
+    subtree: true
   })
 })()
